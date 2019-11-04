7abd11ac20e5e68d424bbd5e68970634741664ae
Valeria Casado <valeria@dolphinnetworks.com.au>
2019-11-04 16:49:36 +0000

    Adjustments

--- src/js/js-elements/Gallery.js
+++ src/js/js-elements/Gallery.js
@@ -5,27 +5,28 @@ class Gallery extends Component {
 
     render() {
         return(
+            <div className="container">
             <div className="gallery">
                 <div style={{backgroundColor: "black", width: "38%", height: "100px"}} className="gallery-item ">
-                <div className="gallery-text"> Hello </div>
+                <div className="gallery-text"><i> " Testimonial one, some text in it, loren impsun et tempus in furias ignemque ruunt: amor omnibus idem. " </i><p className="signature">- Greek person</p></div>
                 </div>
-                <div style={{backgroundColor: "black", width: "40.1%", height: "200px"}} className="i03 gallery-item">
+                <div style={{backgroundColor: "black", width: "40.1%", height: "200px"}} className="o03 gallery-item"> 
 
                 </div>
                 
-                <div style={{backgroundColor: "black", width: "38%", height: "400px", marginTop: "-100px"}} className="i02 gallery-item">
+                <div style={{backgroundColor: "black", width: "38%", height: "400px", marginTop: "-100px"}} className="o02 gallery-item">
 
                 </div>
                 <div style={{backgroundColor: "black", width: "20%", height: "300px", marginTop: ""}} className="gallery-item">
 
                 </div>
-                <div style={{backgroundColor: "black", width: "20%", height: "300px", marginTop: ""}} className="i01 gallery-item">
+                <div style={{backgroundColor: "black", width: "20%", height: "300px", marginTop: ""}} className="o01 gallery-item">
 
                 </div>
-                <div style={{backgroundColor: "black", width: "20%", height: "500px", marginTop: "-200px"}} className="gallery-item">
+                <div style={{backgroundColor: "black", width: "20.88%", height: "500px", marginTop: "-200px"}} className="i02 gallery-item">
                     
                 </div>
-
+                </div>
 
             </div>
         )
